a120e03251b31d117e935f3c67a02edc590f9d73
Anthony Tuininga <anthony.tuininga@gmail.com>
2007-07-24 21:18:47 +0000

    Rename VarcharVar to StringVar.

--- ceODBC.c
+++ ceODBC.c
@@ -220,11 +220,11 @@ void initceODBC(void)
         return;
     if (PyType_Ready(&g_LongBinaryVarType) < 0)
         return;
-    if (PyType_Ready(&g_LongVarcharVarType) < 0)
+    if (PyType_Ready(&g_LongStringVarType) < 0)
+        return;
+    if (PyType_Ready(&g_StringVarType) < 0)
         return;
     if (PyType_Ready(&g_TimestampVarType) < 0)
-        return;
-    if (PyType_Ready(&g_VarcharVarType) < 0)
         return;
 
     // initialize module
@@ -288,9 +288,9 @@ void initceODBC(void)
     ADD_TYPE_OBJECT("DoubleVar", &g_DoubleVarType)
     ADD_TYPE_OBJECT("IntegerVar", &g_IntegerVarType)
     ADD_TYPE_OBJECT("LongBinaryVar", &g_LongBinaryVarType)
-    ADD_TYPE_OBJECT("LongVarcharVar", &g_LongVarcharVarType)
+    ADD_TYPE_OBJECT("LongStringVar", &g_LongStringVarType)
+    ADD_TYPE_OBJECT("StringVar", &g_StringVarType)
     ADD_TYPE_OBJECT("TimestampVar", &g_TimestampVarType)
-    ADD_TYPE_OBJECT("VarcharVar", &g_VarcharVarType)
 
     // add the API types required by the DB API
     CREATE_API_TYPE(g_BinaryApiType, "BINARY")
@@ -308,8 +308,8 @@ void initceODBC(void)
     REGISTER_TYPE(g_NumberApiType, &g_DecimalVarType)
     REGISTER_TYPE(g_NumberApiType, &g_DoubleVarType)
     REGISTER_TYPE(g_NumberApiType, &g_IntegerVarType)
-    REGISTER_TYPE(g_StringApiType, &g_VarcharVarType)
-    REGISTER_TYPE(g_StringApiType, &g_LongVarcharVarType)
+    REGISTER_TYPE(g_StringApiType, &g_StringVarType)
+    REGISTER_TYPE(g_StringApiType, &g_LongStringVarType)
 
     // create constants required by Python DB API 2.0
     if (PyModule_AddStringConstant(module, "apilevel", "2.0") < 0)
