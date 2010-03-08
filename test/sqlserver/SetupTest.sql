c5f5ca2f6860d707dde4a1c63ad9511e3ab9a1aa
Anthony Tuininga <anthony.tuininga@gmail.com>
2010-03-08 16:57:29 +0000

    Added tests for strings, cursors and connections; modified all files to use native line endings since they are text files.

--- test/sqlserver/SetupTest.sql
+++ test/sqlserver/SetupTest.sql
@@ -9,6 +9,18 @@ create table TestNumbers (
     FloatCol            float
 );
 
+create table TestStrings (
+    IntCol              integer not null,
+    StringCol           varchar(20) not null,
+    NullableCol         varchar(50)
+);
+
+create table TestExecuteMany (
+    IntCol              integer not null,
+    StringCol           varchar(50),
+    primary key (IntCol)
+);
+
 delete from TestNumbers;
 
 insert into TestNumbers
@@ -22,6 +34,20 @@ values (3, 9876543210, 37.8);
 
 insert into TestNumbers
 values (4, 98765432101234, 77.27);
+
+delete from TestStrings;
+
+insert into TestStrings
+values (1, 'String 1', null);
+
+insert into TestStrings
+values (2, 'String 2B', 'Nullable One');
+
+insert into TestStrings
+values (3, 'String 3XX', null);
+
+insert into TestStrings
+values (4, 'String 4YYY', 'Nullable Two');
 
 commit;
 
