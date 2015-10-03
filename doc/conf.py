3863310a40deb2f2fb79090ee014aacebecab910
Anthony Tuininga <anthony.tuininga@gmail.com>
2015-10-03 16:43:27 -0600

    Preparing to release 2.1.

--- doc/conf.py
+++ doc/conf.py
@@ -39,9 +39,9 @@ copyright = '2007-2013, Anthony Tuininga'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.1'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.1'
+release = '2.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
